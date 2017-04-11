fb3524873e26ac232063bbad0da43edd458957a1
BrandonGA <brandon.galeaandrews@gmail.com>
2017-04-11 02:14:15 +0000

    fourth commit

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,10 @@
 var request = require('request');
+var fs = require ('fs')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = 'BrandonGA';
 var GITHUB_TOKEN = '7d9aa749fd58449e98bcd6525045a8e959fa1b1b';
-
-
-
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -22,14 +20,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
     if (err) throw err;
     console.log('Response Status Code:', response.statusCode);
     console.log('Content Type: ' + response.headers['content-type'])
-    console.log(JSON.parse(body))
+    var avatarResults = JSON.parse(body)
+    cb(err, avatarResults)
   });
-
 }
 
+function downloadImageByURL(requestURL, filePath) {
+  request.get(requestURL)
+  .on('error', function (err){
+    throw err;
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
+  for (imageURL in result){
+    downloadImageByURL(result[imageURL].avatar_url, './gitHubAvatars/' + result[imageURL].login + '.jpg')
+  }
 });
